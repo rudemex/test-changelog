db669e13eb1c250aec9dbc34adf14fec570d17cd
Mex Delgado <mdelgado@tresdoce.com.ar>
2021-10-31 11:50:53 -0300

    feat(index): add new feature

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const greeting = require('./greeting');
 const feat = require('./my-feature');
 
-console.log("hello world")
+//console.log("hello world")
 greeting.sayHello("mex");
 feat.newFeature("test");
